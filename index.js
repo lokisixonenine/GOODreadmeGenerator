bd6967d597975a9a83dd1c58a4e1f7526d31ee8e
Jaycee Bagtas <jaycee.bagtas@gmail.com>
2020-02-26 08:41:01 -0800

    created index.js
    Created index.js and installed node components

---
+++ index.js
@@ -0,0 +1,7 @@
+const inquirer = require("inquirer");
+    const axios = require("axios");
+    const fs = require("fs");
+    async function main(){
+        console.log(`newReadme`);
+        
+    }
